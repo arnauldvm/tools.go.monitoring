79a86aac36558641d03139a633536c8534030308
Arnauld Van Muysewinkel <arnauldvm@gmail.com>
2016-03-18 23:29:59 +0100

    add(vmstat): add timestamp prefix

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"io"
 	"os"
+	"time"
 
 	"sic.smals.be/tools/monitoring/vmstat"
 )
@@ -19,6 +21,8 @@ func printLine(wt io.WriterTo) {
 	os.Stdout.Write([]byte{'\n'})
 }
 
+const RFC3339Millis = "2006-01-02T15:04:05.000-0700"
+
 func main() {
 	var usage bool
 	flag.BoolVar(&usage, "usage", false, "prints this usage description")
@@ -26,6 +30,7 @@ func main() {
 	periodPtr := flag.Duration("interval", 500e6, "poll interval")
 	durationPtr := flag.Duration("duration", 0, "monitoring duration")
 	cumulPtr := flag.Bool("cumul", false, "log cumulative counters instead of delta")
+	timePtr := flag.Bool("time", true, "add timestamp prefix")
 	flag.Parse()
 	if usage {
 		flag.PrintDefaults()
@@ -33,8 +38,14 @@ func main() {
 	}
 	cout := make(chan vmstat.VmstatRecord)
 	go vmstat.Poll(*periodPtr, *durationPtr, *cumulPtr, cout)
+	if *timePtr {
+		fmt.Print("time", vmstat.Separator)
+	}
 	printLine(vmstat.VmstatHeader)
 	for dat := range cout {
+		if *timePtr {
+			fmt.Print(time.Now().Format(RFC3339Millis), vmstat.Separator)
+		}
 		printLine(dat)
 	}
 }
