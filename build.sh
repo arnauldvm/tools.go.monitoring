6fd28b83ca5f03c7969d03bcd7e62ea46c2ca1e8
Arnauld Van Muysewinkel <Arnauld.VanMuysewinkel@smals.be>
2017-02-16 13:28:16 +0100

    feat(build): add option for cross-compilation

--- build.sh
+++ build.sh
@@ -1,13 +1,45 @@
 #!/bin/sh
 cd $(dirname $0)
-pwd
+
+function usage {
+	echo "Usage: $0 [ target ], with optional target = windows|linux|macos, defaults to local platform"
+}
+
+if [ $# -gt 1 ]; then
+	usage
+	exit 1
+fi
+tgt=bin
+if [ $# -eq 1 ]; then
+	case "$1" in
+	windows)
+	GOOS=windows
+	;;
+	linux)
+	GOOS=linux
+	;;
+	macos)
+	GOOS=darwin
+	;;
+	*)
+	;;
+	esac
+	GOARCH=amd64 # defaults to 64bits!
+	tgt="$tgt/${GOOS}_${GOARCH}"
+	export GOOS GOARCH
+	echo "Building for ${GOOS}_${GOARCH} architecture"
+else
+	echo "Building for default(local) architecture"
+fi
+mkdir -p "$tgt"
+
 GOPATH="$(pwd)"
-mkdir -p bin
 opt=
 #opt="$opt -x" # verbose
+
 for tool in src/cmd/*; do
     name="$(basename "$tool")"
     echo Building $name
-    go build $opt -o bin/$name $tool/main.go
+    go build $opt -o "$tgt/$name" $tool/main.go
 done
 echo Done.
